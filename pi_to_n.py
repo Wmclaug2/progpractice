12175736e2fabbf67a082c4bc275175d39398d9e
wmclaug2 <wmclaug2@gmail.com>
2017-09-03 21:33:03 -0400

    Complete pi to n

--- pi_to_n.py
+++ pi_to_n.py
@@ -1,19 +1,40 @@
 """
 Heres your docstring pylint >:(
+    get_input
+
 """
 import math
 
 def get_input():
-    print 'How many digits after the decimal would you like to print Pi?'
-    index = raw_input()
+    """
+    Gets an input
+    """
+    while True:
+        try:
+            index = int(raw_input("How many digits of PI do you want to print?"))
+        except ValueError:
+            print "Sorry, that was not a valid number..."
+            continue
+        else:
+            if index <= 50:
+                break
+            else:
+                print "Please enter a digit less than 50."
+                continue
     return int(index)
 
 def get_pi_to_digit(i):
-    return round(math.pi, i)
+    """
+    Returns Pi to specified index
+    """
+    return '{:.{prec}f}'.format(math.pi, prec=i)
 
-def print_result(res):
-    s = ("The precision of PI you asked for is %f!" % res)
-    print s
+def print_result(response):
+    """
+    Prints the result
+    """
+    formatted_response = ("The precision of PI you asked for is %s!" % response)
+    print formatted_response
 
 print_result(get_pi_to_digit(get_input()))
 print "\n"
